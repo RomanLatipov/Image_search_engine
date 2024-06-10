c4614f38c285b404cb4e23d276a8433d6d48d244
romanlatipov <romanlatipov@gmail.com>
2024-06-10 14:59:03 -0400

    migrate weaviate to v3

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-import weaviate from 'weaviate-ts-client';
+import weaviate from 'weaviate-client'
 import fs from 'fs'
 
-const client = weaviate.client({
-    scheme: 'http',
-    host: 'localhost:8080',
-});
+const client = await weaviate.connectToLocal()
+ 
+const myCollection = client.collections.get('Images');
+const contentsBase64 = await fs.promises.readFile('./test.jpg', { encoding: 'base64' });
+const result = await myCollection.query.nearImage(contentsBase64, {
+  returnProperties: ['image'],
+  limit: 3,
+})
+// console.log(result.objects[0].properties.image);
 
-const test = Buffer.from(fs.readFileSync('./test.jpg')).toString('base64');
-
-const resImage = await client.graphql.get().withClassName('Images').withFields(['image']).withNearImage({image: test}).withLimit(3).do();
-
-let result;
 for (let i=0; i<3; i++){
-    result = resImage.data.Get.Images[i].image;
-    fs.writeFileSync(`./result${i}.jpg`, result, 'base64');
+    fs.writeFileSync(`./result${i}.jpg`, result.objects[i].properties.image, 'base64');
 }
+
+for await (let item of myCollection.iterator()) {
+  console.log(item.uuid, item.properties);
+}
+
+// const img = fs.readFileSync('images/download-2.jpg');
+// const b64 = Buffer.from(img).toString('base64');
+// const dataObj = {
+//     image: b64,
+//     text : 'download-2'
+// }
+// console.log(generateUuid5(JSON.stringify(dataObj)))
+
+// import weaviate from 'weaviate-ts-client';
+// import fs from 'fs'
+
+// const client = weaviate.client({
+//     scheme: 'http',
+//     host: 'localhost:8080',
+// });
+
+// const test = Buffer.from(fs.readFileSync('./test.jpg')).toString('base64');
+
+// const resImage = await client.graphql.get().withClassName('Images').withFields(['image']).withNearImage({image: test}).withLimit(3).do();
+
+// let result;
+// for (let i=0; i<3; i++){
+//     result = resImage.data.Get.Images[i].image;
+//     fs.writeFileSync(`./result${i}.jpg`, result, 'base64');
+// }
+
+// const myCollection = client.collections.get('Images');
--- weaviate.js
+++ weaviate.js
@@ -1,10 +1,7 @@
-import weaviate from 'weaviate-ts-client';
+import { weaviate, generateUuid5 } from 'weaviate-client';
 import fs from 'fs';
 
-const client = weaviate.client({
-    scheme: 'http',
-    host: 'localhost:8080',
-});
+const client = await weaviate.connectToLocal()
 
 // const schemaRes = await client.schema.getter().do();
 
@@ -15,15 +12,23 @@ const client = weaviate.client({
 //     text: 'test'
 // }).do();
 
+const myCollection = client.collections.get('Images');
+const idArr = [];
+for await (let item of myCollection.iterator()) {
+    idArr.push(item.uuid);
+}
+
 const imgFiles = fs.readdirSync('./images');
 // console.log(imgFiles)
 const promises = imgFiles.map(async (imgFile) => {
     const b64 = Buffer.from(fs.readFileSync(`images/${imgFile}`)).toString('base64');
-
-    await client.data.creator().withClassName("Images").withProperties({
+    const dataObj = {
         image: b64,
         text : imgFile.split('.')[0].split('_').join(' ')
-    }).do();
+    }
+    const uuid = generateUuid5(JSON.stringify(dataObj))
+    if (!idArr.includes(uuid))
+        await client.data.creator().withClassName("Images").withProperties(dataObj).withId(uuid).do();
 })
 
 await Promise.all(promises);
